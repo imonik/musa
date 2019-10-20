23f332e16f086b5e7b00caae0568cbdebdb69a7a
Monik de Santiago <mk.santb@gmail.com>
2019-10-20 10:40:19 -0700

    added last changes for images and andjustmests

---
+++ assets/js/onload.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // var firstSite = sessionStorage['firstSite'];
+    // if (!firstSite) { // or firstSite != "visited"
+
+        // some code here if the user is new to the site
+        // $('#exampleModalCenter').modal('show');
+
+        // sessionStorage['firstSite'] = "visited";
+    // }
+});
